02a20c26e4f201419d6acd4bbfcb22ea8e5c0bc2
Daniel Helfrich <daniel.helfrich@colorado.edu>
2014-03-17 13:36:33 -0600

    more notes

--- Notes/L27.java
+++ Notes/L27.java
@@ -1,17 +1,37 @@
 
 public class L27 {
 
-	/**
-	 * @param args
-	 */
+	public static void printMessage(String msg){
+		System.out.println(msg);
+	}
+	
+	
 	public static void main(String[] args) {
 		int [][]int2DArray = {{1,2,3,4},{5,6,7,8}};
 		//need a loop for each dimension
 		for(int x = 0; x < int2DArray.length; x++){
-			for(int y = 0; y < int2DArray.length; y++){
+			for(int y = 0; y < int2DArray[x].length; y++){
 				System.out.println(int2DArray[x][y]);
 			}
 		}
+				
+				
+		String[] strArray = {"good job", "hang in there", "spring break soon"};
+		for(int z = 0; z < strArray.length; z++){
+			printMessage(strArray[z]);
+			}
+		
+		double area = calcArea(4);
+		System.out.println("The area is " + area);
+		
+	}
+	
+	//write a method that takes one argument, the radius and returns a double
+	
+	public static double calcArea(double radius) {
+		double area = Math.PI*Math.pow(radius, 2);
+		return area;
+				
 	}
 
 }
