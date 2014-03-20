8ac8658cb890b341d69895da8e3ffb6a0dd5dd26
Daniel Helfrich <daniel.helfrich@colorado.edu>
2014-03-20 12:19:16 -0600

    recitation 9

---
+++ Notes/Recitation9.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class Recitation9 {
+	
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int i = sc.nextInt();
+		System.out.println(i);
+		
+		String line = sc.nextLine();
+		System.out.println(line);
+		
+		sc.close();
+	}
+
+}
